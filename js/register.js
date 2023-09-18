61a4949360ca1cd637573f2d08c0f1425a515b7b
smich254 <smich254@gmail.com>
2023-09-18 14:59:25 -0500

    Agregado logica register.js

--- js/register.js
+++ js/register.js
@@ -2,18 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const registerButton = document.getElementById("registerButton");
 
   registerButton.addEventListener("click", function () {
-    const nombre = document.getElementById("name").value;
-    const apellido = document.getElementById("lastName").value;
-    const usuario = document.getElementById("userName").value;
-    const correo = document.getElementById("email").value;
-    const contrasena = document.getElementById("password").value;
+    const name = document.getElementById("name").value;
+    const lastName = document.getElementById("lastName").value;
+    const userName = document.getElementById("userName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const isAdmin = false;
 
     fetch("http://localhost:3000/api/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, lastName, userName, email, password }),
+      body: JSON.stringify({
+        name,
+        lastName,
+        userName,
+        email,
+        password,
+        isAdmin,
+      }),
     })
       .then((response) => response.json())
       .then((data) => {
